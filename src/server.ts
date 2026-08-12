9f7083021be186f7b9c3482868390eeea5ccf296
Mateus R. Pereira <professor.mateusramos@gmail.com>
2026-08-11 21:40:16 -0300

    Aula 1.1 - primeiras rotas com HTTP

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,41 @@
 import http from "node:http";
 
 const port = 3000;
-const server = http.createServer((req, res) => {
-    const response = {
-        message: "API Restaurante",
-        version: "1.0.0",
-    };
 
-    res.writeHead(200, {
-        "content-type": "application/json",
+const routes = {
+  "/": {
+    message: "Restaurant Ordering System API",
+  },
+  "/categories": {
+    message: "Lista de Categorias",
+  },
+  "/products": {
+    message: "Lista de produtos",
+  },
+};
+
+const server = http.createServer((req, res) => {
+  const response = routes[req.url as keyof typeof routes];
+
+  if (!response) {
+    res.writeHead(404, {
+      "content-type": "application/json",
     });
 
-    res.end(JSON.stringify(response));
+    return res.end(
+      JSON.stringify({
+        message: "Rota não encontrada",
+      }),
+    );
+  }
+
+  res.writeHead(200, {
+    "content-type": "application/json",
+  });
+
+  res.end(JSON.stringify(response));
 });
 
 server.listen(port, () => {
-    console.log(`Servidor executando em http://localhost:${port}`);
-})
+  console.log(`Servidor executando em http://localhost:${port}`);
+});
