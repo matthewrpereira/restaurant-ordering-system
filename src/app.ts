2e599433aa7d95982fcbf561161481b5fbac68ce
Mateus <professor.mateusramos@gmail.com>
2026-08-18 21:02:15 -0300

    Aula 2: criação de recursos com post

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,9 @@
 import express from "express";
 
 const app = express();
+
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.status(200).json({
     message: "Restaurant Ordering System API",
@@ -8,16 +11,93 @@ app.get("/", (req, res) => {
   });
 });
 
+const categories = [
+  {
+    id: 1,
+    name: "Pizzas",
+    description:
+      "Pizzas salgadas com diferentes sabores, tamanhos e combinações de ingredientes.",
+  },
+  {
+    id: 2,
+    name: "Bebidas",
+    description:
+      "Bebidas para acompanhar as pizzas, incluindo refrigerantes, sucos e água.",
+  },
+  {
+    id: 3,
+    name: "Sobremesas",
+    description:
+      "Opções doces para finalizar a refeição, como pizzas doces, sorvetes e sobremesas especiais.",
+  },
+];
+
+const products = [
+  {
+    id: 1,
+    categoryId: 1,
+    name: "Calabresa",
+    description:
+      "Pizza com molho de tomate, muçarela, calabresa fatiada e cebola.",
+    price: 45.9,
+  },
+  {
+    id: 2,
+    categoryId: 1,
+    name: "Frango com Catupiry",
+    description:
+      "Pizza com molho de tomate, muçarela, frango desfiado e catupiry.",
+    price: 49.9,
+  },
+  {
+    id: 3,
+    categoryId: 1,
+    name: "Margherita",
+    description:
+      "Pizza com molho de tomate, muçarela, tomate e manjericão fresco.",
+    price: 44.9,
+  },
+  {
+    id: 4,
+    categoryId: 2,
+    name: "Coca-Cola 2L",
+    description: "Refrigerante Coca-Cola em garrafa de 2 litros.",
+    price: 12.9,
+  },
+  {
+    id: 5,
+    categoryId: 2,
+    name: "Suco de Laranja",
+    description: "Suco natural de laranja, servido gelado.",
+    price: 9.9,
+  },
+  {
+    id: 6,
+    categoryId: 3,
+    name: "Pizza de Chocolate",
+    description: "Pizza doce com chocolate cremoso e granulado.",
+    price: 39.9,
+  },
+];
+
 app.get("/categories", (req, res) => {
-  res.status(200).json({
-    message: "Lista de Categorias",
-  });
+  res.status(200).json(categories);
+});
+
+app.post("/categories", (req, res) => {
+  const category = req.body;
+  categories.push(category);
+  res.status(201).json(category);
 });
 
 app.get("/products", (req, res) => {
-  res.status(200).json({
-    message: "Lista de produtos",
-  });
+  res.status(200).json(products);
+});
+
+app.post("/products", (req, res) => {
+  const product = req.body;
+  products.push(product);
+  res.status(201).json(product);
 });
 
 export default app;
