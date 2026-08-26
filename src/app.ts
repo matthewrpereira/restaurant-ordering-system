7fdf905ceec0a9d2bd1c679f7e1a3262ab675b29
Mateus R. Pereira <professor.mateusramos@gmail.com>
2026-08-25 21:28:59 -0300

    feat: Aula 3 - CRUD completo de Categorias e Produtos

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,77 @@ import express from "express";
 import { randomUUID } from "node:crypto";
 
 const app = express();
-
 app.use(express.json());
 
-//const pizzaCategoryId = randomUUID();
-//const drinkCategoryId = randomUUID();
+const pizzaCategoryId = randomUUID();
+const drinkCategoryId = randomUUID();
 
+const categories = [
+  {
+    id: pizzaCategoryId,
+    name: "Pizzas",
+    description:
+      "Pizzas salgadas com diferentes sabores, tamanhos e combinações de ingredientes.",
+  },
+  {
+    id: drinkCategoryId,
+    name: "Bebidas",
+    description:
+      "Bebidas para acompanhar as pizzas, incluindo refrigerantes, sucos e água.",
+  },
+];
+
+const products = [
+  {
+    id: randomUUID(),
+    categoryId: pizzaCategoryId,
+    name: "Calabresa",
+    description:
+      "Pizza com molho de tomate, muçarela, calabresa fatiada e cebola.",
+    price: 45.9,
+  },
+  {
+    id: randomUUID(),
+    categoryId: pizzaCategoryId,
+    name: "Frango com Catupiry",
+    description:
+      "Pizza com molho de tomate, muçarela, frango desfiado e catupiry.",
+    price: 49.9,
+  },
+  {
+    id: randomUUID(),
+    categoryId: pizzaCategoryId,
+    name: "Margherita",
+    description:
+      "Pizza com molho de tomate, muçarela, tomate e manjericão fresco.",
+    price: 44.9,
+  },
+  {
+    id: randomUUID(),
+    categoryId: drinkCategoryId,
+    name: "Coca-Cola 2L",
+    description: "Refrigerante Coca-Cola em garrafa de 2 litros.",
+    price: 12.9,
+  },
+  {
+    id: randomUUID(),
+    categoryId: drinkCategoryId,
+    name: "Suco de Laranja",
+    description: "Suco natural de laranja, servido gelado.",
+    price: 9.9,
+  },
+  {
+    id: randomUUID,
+    categoryId: pizzaCategoryId,
+    name: "Pizza de Chocolate",
+    description: "Pizza doce com chocolate cremoso e granulado.",
+    price: 39.9,
+  },
+];
+
+// ==========================
+// Root
+// ==========================
 app.get("/", (req, res) => {
   res.status(200).json({
     message: "Restaurant Ordering System API",
@@ -15,75 +80,9 @@ app.get("/", (req, res) => {
   });
 });
 
-const categories = [
-  {
-    id: randomUUID(),
-    name: "Pizzas",
-    description:
-      "Pizzas salgadas com diferentes sabores, tamanhos e combinações de ingredientes.",
-  },
-  {
-    id: randomUUID(),
-    name: "Bebidas",
-    description:
-      "Bebidas para acompanhar as pizzas, incluindo refrigerantes, sucos e água.",
-  },
-  {
-    id: randomUUID(),
-    name: "Sobremesas",
-    description:
-      "Opções doces para finalizar a refeição, como pizzas doces, sorvetes e sobremesas especiais.",
-  },
-];
-
-const products = [
-  {
-    id: 1,
-    categoryId: 1,
-    name: "Calabresa",
-    description:
-      "Pizza com molho de tomate, muçarela, calabresa fatiada e cebola.",
-    price: 45.9,
-  },
-  {
-    id: 2,
-    categoryId: 1,
-    name: "Frango com Catupiry",
-    description:
-      "Pizza com molho de tomate, muçarela, frango desfiado e catupiry.",
-    price: 49.9,
-  },
-  {
-    id: 3,
-    categoryId: 1,
-    name: "Margherita",
-    description:
-      "Pizza com molho de tomate, muçarela, tomate e manjericão fresco.",
-    price: 44.9,
-  },
-  {
-    id: 4,
-    categoryId: 2,
-    name: "Coca-Cola 2L",
-    description: "Refrigerante Coca-Cola em garrafa de 2 litros.",
-    price: 12.9,
-  },
-  {
-    id: 5,
-    categoryId: 2,
-    name: "Suco de Laranja",
-    description: "Suco natural de laranja, servido gelado.",
-    price: 9.9,
-  },
-  {
-    id: 6,
-    categoryId: 3,
-    name: "Pizza de Chocolate",
-    description: "Pizza doce com chocolate cremoso e granulado.",
-    price: 39.9,
-  },
-];
-
+// ==========================
+// Categories
+// ==========================
 app.get("/categories", (req, res) => {
   res.status(200).json(categories);
 });
@@ -103,19 +102,112 @@ app.get("/categories/:id", (req, res) => {
 });
 
 app.post("/categories", (req, res) => {
-  const category = req.body;
+  const category = {
+    id: randomUUID(),
+    ...req.body,
+  };
   categories.push(category);
   res.status(201).json(category);
 });
 
+app.put("/categories/:id", (req, res) => {
+  const category = categories.find((category) => {
+    return category.id == req.params.id;
+  });
+
+  if (!category) {
+    return res.status(404).json({
+      message: "Categoria não encontrada.",
+    });
+  }
+
+  category.name = req.body.name;
+  category.description = req.body.description;
+
+  res.status(200).json(category);
+});
+
+app.delete("/categories/:id", (req, res) => {
+  const category = categories.find((category) => {
+    return category.id == req.params.id;
+  });
+
+  if (!category) {
+    return res.status(404).json({
+      message: "Categoria não encontrada.",
+    });
+  }
+
+  const index = categories.indexOf(category);
+  categories.splice(index, 1);
+
+  res.status(200).json({
+    message: "Categoria removida com sucesso.",
+  });
+});
+
+// ==========================
+// Products
+// ==========================
 app.get("/products", (req, res) => {
   res.status(200).json(products);
 });
 
 app.post("/products", (req, res) => {
-  const product = req.body;
+  const product = {
+    id: randomUUID(),
+    ...req.body,
+  };
   products.push(product);
   res.status(201).json(product);
+});
+
+app.get("/products/:id", (req, res) => {
+  const product = products.find((product) => {
+    return product.id == req.params.id;
+  });
+  if (!product) {
+    return res.status(404).json({
+      message: "Produto não encontrado.",
+    });
+  }
+  res.status(200).json(product);
+});
+
+app.put("/products/:id", (req, res) => {
+  const product = products.find((product) => {
+    return product.id == req.params.id;
+  });
+  if (!product) {
+    return res.status(404).json({
+      message: "Produto não encontrado.",
+    });
+  }
+
+  product.categoryId = req.body.categoryId;
+  product.name = req.body.name;
+  product.description = req.body.description;
+  product.price = req.body.price;
+
+  res.status(200).json(product);
+});
+
+app.delete("/products/:id", (req, res) => {
+  const product = products.find((product) => {
+    return product.id == req.params.id;
+  });
+  if (!product) {
+    return res.status(404).json({
+      message: "Produto não encontrado.",
+    });
+  }
+
+  const index = products.indexOf(product);
+  products.splice(index, 1);
+
+  res.status(200).json({
+    message: "Produto removido com sucesso.",
+  });
 });
 
 export default app;
